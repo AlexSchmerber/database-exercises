8448bfd39cc604688196922a2a83917618b6a37d
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-03-01 15:56:50 -0600

    mid delete-exercises.sql

---
+++ select_exercises.sql
@@ -0,0 +1,5 @@
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date FROM albums WHERE album_name = 'Sgt. Peppers Lonely Hearts Club Band';
+SELECT * FROM albums WHERE release_date > 1989 AND release_date < 2000;
+SELECT * FROM albums WHERE sales < 20.000000;
+SELECT * FROM albums WHERE genre = "Rock";
