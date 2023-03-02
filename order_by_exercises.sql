c3bcb2d763ee9bd7415ec12ec45aab14d6a9b2b1
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-03-02 11:45:09 -0600

    post order_by_exercises.sql

---
+++ order_by_exercises.sql
@@ -0,0 +1,33 @@
+USE ymir_employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%Q%';
+
+SELECT *
+FROM employees
+WHERE gender = 'M' AND first_name = 'Irena' OR gender = 'M' AND first_name = 'Vidya' OR gender = 'M' AND first_name = 'Maya';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%E'
+   OR last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND NOT last_name LIKE '%qu%';
